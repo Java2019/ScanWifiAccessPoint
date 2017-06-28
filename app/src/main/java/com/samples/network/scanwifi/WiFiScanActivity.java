845cdef7bbc08bb15b907efc6a3776bd513175b0
java2019 <java_2019@mail.ru>
2017-06-28 10:26:56 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/network/scanwifi/WiFiScanActivity.java
@@ -0,0 +1,13 @@
+package com.samples.network.scanwifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WiFiScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wi_fi_scan);
+    }
+}
