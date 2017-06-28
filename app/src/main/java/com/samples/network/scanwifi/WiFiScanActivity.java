e6f5f76b5fdc212f6615e44fc268728346e17f88
java2019 <java_2019@mail.ru>
2017-06-28 11:04:58 +0300

    test1

--- app/src/main/java/com/samples/network/scanwifi/WiFiScanActivity.java
+++ app/src/main/java/com/samples/network/scanwifi/WiFiScanActivity.java
@@ -1,13 +1,51 @@
 package com.samples.network.scanwifi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
-public class WiFiScanActivity extends AppCompatActivity {
+public class WiFiScanActivity extends AppCompatActivity
+        implements CompoundButton.OnCheckedChangeListener, View.OnClickListener{
+
+    private TextView text;
+    private CheckBox checkBox;
+    private Button button;
+
+    private WifiManager manager;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wi_fi_scan);
+
+        text = (TextView)findViewById(R.id.text);
+        checkBox =  (CheckBox)findViewById(R.id.checkbox);
+        button = (Button)findViewById(R.id.bStart);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
     }
 }
