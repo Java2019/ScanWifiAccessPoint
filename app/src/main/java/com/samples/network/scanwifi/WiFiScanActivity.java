ad73bfb4f2b89605e5918265f3e0d6acc6355974
java2019 <java_2019@mail.ru>
2017-06-28 11:13:47 +0300

    test2

--- app/src/main/java/com/samples/network/scanwifi/WiFiScanActivity.java
+++ app/src/main/java/com/samples/network/scanwifi/WiFiScanActivity.java
@@ -3,6 +3,7 @@ package com.samples.network.scanwifi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class WiFiScanActivity extends AppCompatActivity
         implements CompoundButton.OnCheckedChangeListener, View.OnClickListener{
@@ -24,7 +27,40 @@ public class WiFiScanActivity extends AppCompatActivity
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+            switch (wifiState){
+                case WifiManager.WIFI_STATE_ENABLING:
+                    text.setText("Wi-fi state enabling");
+                    break;
+                case WifiManager.WIFI_STATE_ENABLED:
+                    text.setText("Wi-fi state enabled");
+                    break;
+                case WifiManager.WIFI_STATE_DISABLING:
+                    text.setText("Wi-fi state disabling");
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    text.setText("Wi-fi state disabled");
+                    break;
+                case WifiManager.WIFI_STATE_UNKNOWN:
+                    text.setText("Wi-fi state unknown");
+                    break;
+            }
+        }
+    };
 
+    private BroadcastReceiver scanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> results = manager.getScanResults();
+            text.setText("Scan result: " + results.size() + " points");
+            // Выводим результаты сканирования на экран
+            for (ScanResult result : results) {
+                text.append("\nSS ID:\t" + result.SSID);
+                text.append("\n\tLevel:\t" + result.level + " dBm");
+                text.append("\n\tFrequency:\t" + result.frequency + " MHz");
+                text.append("\n\tCapabilities:\t" + result.capabilities);
+            }
         }
     };
 
